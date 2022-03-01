3046f1e3bcf8d8ecfaa54f9c37e7dbaf0609d6a9
dunglv <dunglv3@itsol.vn>
2022-03-01 17:19:57 +0700

    config_register_download_jar_google

---
+++ AppBookingRoom/app/src/main/java/com/example/appbookingroom/activity/ActivityForgotPassword.java
@@ -0,0 +1,16 @@
+package com.example.appbookingroom.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appbookingroom.R;
+
+public class ActivityForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
