216076c5db858479c87b2056ca1d24d0084d4b56
dunglv <dunglv3@itsol.vn>
2022-03-01 10:34:40 +0700

    created-package-common

---
+++ AppBookingRoom/app/src/main/java/com/example/appbookingroom/common/Constants.java
@@ -0,0 +1,13 @@
+package com.example.appbookingroom.common;
+
+public class Constants {
+    public static final String IP_ADDRESS = "172.30.128.1";
+
+
+
+
+
+    public static class API {
+        public static final String URL_CREATE_USER = "http://" + IP_ADDRESS + "/api/customer/created";
+    }
+}
