b5a8e5e24c7a322cbebe6adae1a87f33aec2ae11
dunglv <dunglv3@itsol.vn>
2022-03-02 17:52:22 +0700

    config-map

--- AppBookingRoom/app/src/main/java/com/example/appbookingroom/activity/ActivityInfoHotel.java
+++ AppBookingRoom/app/src/main/java/com/example/appbookingroom/activity/ActivityInfoHotel.java
@@ -1,5 +1,6 @@
 package com.example.appbookingroom.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -8,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.appbookingroom.R;
 
 public class ActivityInfoHotel extends AppCompatActivity {
-    private TextView description,txtReadMore;
+    private TextView description,txtReadMore,txtAddressForHotel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +20,16 @@ public class ActivityInfoHotel extends AppCompatActivity {
     }
     private void init(){
         description= findViewById(R.id.description);
+        txtAddressForHotel = findViewById(R.id.txtAddressForHotel);
 //        description.setMovementMethod(new ScrollingMovementMethod());
         txtReadMore = findViewById(R.id.readMore);
     }
     private void event(){
         txtReadMore.setOnClickListener(v->{
             description.setMaxLines(Integer.MAX_VALUE);
+        });
+        txtAddressForHotel.setOnClickListener(v->{
+            startActivity(new Intent(getApplicationContext(),MapsActivity.class));
         });
     }
 }
